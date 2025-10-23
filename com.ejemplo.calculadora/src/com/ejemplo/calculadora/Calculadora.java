876635f78de07ad8a69755157d7eaf9d9e4243af
Juanlo Saez <jsaesic2312@g.educaan.es>
2025-10-23 10:48:11 +0200

    "Versión inicial: clase Calculadora con operación suma"

---
+++ com.ejemplo.calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package com.ejemplo.calculadora;
+
+public class Calculadora {
+	public static void main (String[] args){
+		
+		
+	}
+	public class calculadora{
+		public double sumar(double a, double b) {
+			return a + b;
+		}
+	}	
+}
---
+++ com.ejemplo.calculadora/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module com.ejemplo.calculadora {
+}
